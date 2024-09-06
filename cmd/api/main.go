ccb72858fa12aca6f14b0c46461f6200434d3c66
Jorge Reyes <92123185+jorgeart81@users.noreply.github.com>
2024-09-06 18:36:37 -0500

    feat: add routes.go

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,3 +1,30 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+const webPort = 80
+
+type Config struct{}
+
+func main() {
+	app := Config{}
+
+	log.Printf("Starting broker service on port %d\n", webPort)
+
+	// define http server
+	srv := &http.Server{
+		Addr:    fmt.Sprintf(":%d", webPort),
+		Handler: app.routes(),
+	}
+
+	// start to server
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Panic(err)
+	}
+
+}
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module broker
 
 go 1.22.5
 
-require github.com/go-chi/chi/v5 v5.1.0 // indirect
+require (
+	github.com/go-chi/chi/v5 v5.1.0 // indirect
+	github.com/go-chi/cors v1.2.1 // indirect
+)
